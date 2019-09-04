05e3a828bbc95258a015f80d111fd15584c365d1
alex.afanasev <Alex.Afanasev@stud.uni-goettingen.de>
2019-09-04 20:56:59 +0200

    Added package help file.

---
+++ R/NeuralNetworkVisualization.R
@@ -0,0 +1,8 @@
+#' NeuralNetworkVisualization: Create Partial Dependence Plots for Neural Networks. Visualizes the marginal effects of Neural Networks.
+#'
+#' NeuralNetworkVisualization is a R Package for creating beautiful partial
+#'   dependence plots for Neural Networks using ggplot2, plotly and shiny.
+#'
+#' @docType package
+#' @name NeuralNetworkVisualization
+NULL
